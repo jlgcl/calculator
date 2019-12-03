f166559cf0371a60a522e59334d0676909f07332
James <jwljacl@gmail.com>
2019-12-02 23:19:11 -0500

    rev0

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let add = function(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+let subtract = function(num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+let multiply = function(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+let divide = function(num1, num2) {
+    let result = num1 / num2;
+    return result;
+}
+
+let operator = function(num1, num2) {
+    add(num1, num2);
+    subtract(num1, num2);
+    multiply(num1, num2);
+    divide(num1, num2);
+}
