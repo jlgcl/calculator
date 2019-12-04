8a0d062f68f5d12d63c5da7584f8e66fa23a5510
James <jwljacl@gmail.com>
2019-12-03 23:25:17 -0500

    rev1

--- calculator.js
+++ calculator.js
@@ -1,3 +1,24 @@
+/* THE ODIN PROJECT - WEB DEV 101 - CALCULATOR
+
+APPROACH:
+    1) inside operator(num1, num2), I must have condition: if a operator key is pressed, perform any of operation.
+    2) create a function that translates key/button entry to display & store values.
+
+METHODS:
+    1) onclick at HTML buttons?
+
+STATUS:
+    - Find a way to display value when button clicked.
+
+*/
+
+var numberBut = document.getElementsByClassName("numButtons");
+var opBut = document.getElementsByClassName("opButtons");
+var disp = document.getElementById("display");
+var clr = document.getElementById("AC");
+var eql = document.getElementById("equals");
+
+
 let add = function(num1, num2) {
     let result = num1 + num2;
     return result;
@@ -18,9 +39,21 @@ let divide = function(num1, num2) {
     return result;
 }
 
-let operator = function(num1, num2) {
+let operator = function(num1, num2) { 
+    
     add(num1, num2);
     subtract(num1, num2);
     multiply(num1, num2);
     divide(num1, num2);
 }
+
+let perform = function () {
+    let arr = [];
+
+    document.getElementById("one").addEventListener("click", function() {
+        disp.innerHTML = 1;
+        //arr.push(1);
+    });
+}
+
+perform();
